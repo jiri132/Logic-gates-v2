4dc4a57755b852be785b564fbb89a4e07ad2494e
jiri132 <jiri28beurskens@gmail.com>
2022-11-05 13:17:27 +0100

    Chore: Added todo + Generic typing

--- Assets/Scripts/Extentions/Observables/Observable.cs
+++ Assets/Scripts/Extentions/Observables/Observable.cs
@@ -7,11 +7,11 @@ namespace Extentions.Observables
 {
     //TODO: Make the observable be able to subscrie
     //TODO: Make the observables generic
-    public class Observable
+    public class Observable<T>
     {
         public event EventHandler SomethingHappened;
 
-        
+        //TODO: Change function name to something more applicable
         public void DoSomething() => SomethingHappened?.Invoke(this, EventArgs.Empty);
     }
 }
