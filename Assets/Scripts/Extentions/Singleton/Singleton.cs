3682ec0f44e5bbe83462ec65a4a2fce43e9cc473
jiri132 <76393975+jiri132@users.noreply.github.com>
2022-11-14 11:40:47 +0100

    Chore: Do all TODO

--- Assets/Scripts/Extentions/Singleton/Singleton.cs
+++ Assets/Scripts/Extentions/Singleton/Singleton.cs
@@ -1,10 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour
+public class Singleton<T> : MonoBehaviour where T : Component
 {
-    //TODO: Make the extention of singleotn
-    //TODO: Use generic typing
-    //TODO: keep it alive or destroy depends on new scenes
+    protected static T _instance;
+
+    /// <summary>
+    /// Singleton design pattern
+    /// </summary>
+    /// <value>The instance.</value>
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    //Debug.Log("Creating a Singleton instance of " + typeof(T).ToString());
+                    GameObject obj = new GameObject(typeof(T).ToString());
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    /// <summary>
+    ///// On awake, we initialize our instance. Make sure to call base.Awake() in override if you need awake.
+    /// </summary>
+    protected virtual void Awake()
+    {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+
+        _instance = this as T;
+    }
 }
