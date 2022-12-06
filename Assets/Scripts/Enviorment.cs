988474341284a0e5fe48830a8e78bd0230f02e6b
jiri132 <76393975+jiri132@users.noreply.github.com>
2022-12-07 00:55:00 +0100

    Rework: Enviorment takes over gamemanager in gates building

--- Assets/Scripts/Enviorment.cs
+++ Assets/Scripts/Enviorment.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enviorment : MonoBehaviour
 {
-    public string name = "";
+    public InputField gateNameInput;
     public Color randomColor;
     private void Start()
     {
         randomColor = new Color(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-        SaveSystem1.SaveGate(new GateData(this), name);
+       
     }
-
 
     public void CreateNewGate()
     {
-        SaveSystem1.SaveGate(new GateData(this), name);
+        SaveSystem1.SaveGate(new GateData(this), gateNameInput.text);
     }
 
 }
