d4d4dcffec02db201bec500bfc9ff9331c0aa7a9
jiri132 <76393975+jiri132@users.noreply.github.com>
2022-12-07 00:54:37 +0100

    Feature: Added basic loading of the custom gate

--- Assets/Scripts/LogicGate/new/CUSTOMGate.cs
+++ Assets/Scripts/LogicGate/new/CUSTOMGate.cs
@@ -1,50 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Logic.Nodes;
+
 
 namespace Logic
 {
     public class CUSTOMGate : LogicComponent
     {
-        #region Constructor
+        [Header("Base variable")]
+        public string fileName;
+        public Color gateColor;
 
-        public CUSTOMGate(int inputs, int outputs, string name)
+
+        [Header("Prefabs")]
+        public NOTGate prefabNOT;
+        public ANDGate prefabAND;
+
+        [Header("SavedData")]
+        public GateData DATA;
+        public List<LogicComponent> AllGatesForCustomGate = new List<LogicComponent>();
+
+        private void Start()
         {
-            base.name = name;
-           // base.outputs = new byte[outputs];
-        }
+            DATA = SaveSystem1.LoadGate(fileName);
 
-        #endregion
+            gateColor = new Color(DATA.rgb[0], DATA.rgb[1], DATA.rgb[2]);
 
-        #region Variables
-
-
-
-        #endregion
-
-        public void Start()
-        {
-            /*for (int i = 0; i < link.Length; i++)
+            //add the gates in the order of instantiating
+            for (int i = 0; i < DATA.NumberOfGates; i++)
             {
-                link[i]._self = this;
-                link[i]._outputIndex = i;
-            }*/
+                switch (DATA.GateSpawnFormat[i])
+                {
+                    case TYPES.NOT:
+                        LogicComponent not = this.gameObject.AddComponent(typeof(NOTGate)) as NOTGate;
+                        not.Local_ID = AllGatesForCustomGate.Count;
+                        AllGatesForCustomGate.Add(not);
+                        break;
+                    case TYPES.AND:
+                        LogicComponent and = this.gameObject.AddComponent(typeof(ANDGate)) as ANDGate;
+                        and.Local_ID = AllGatesForCustomGate.Count;
+                        AllGatesForCustomGate.Add(and);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
+            for (int i = 0; i < DATA.Connections.Count; i++)
+            {
+                int ID = DATA.Connections[i].Item1;
+                int IDOutput = DATA.Connections[i].Item2;
+                int ConnectionID = DATA.Connections[i].Item3;
+                int ConnectionIDInput = DATA.Connections[i].Item4;
+
+                OutputNode node = AllGatesForCustomGate[ID].outputs[IDOutput] as OutputNode;
+                node.Links.CreateRelation(AllGatesForCustomGate[ConnectionID].inputs[ConnectionIDInput]);
+            }
+
+
         }
-
-        #region Overrides Of Component
-
-        private void Awake()
-        {
-            //TODO: make a save system for custom gates data
-            //base.Setup("CUSTOM",this,);
-        }
-
-        public override void Propegation()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        #endregion
     }
 }
 
