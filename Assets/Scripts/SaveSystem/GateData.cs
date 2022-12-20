3a58cdcadbe3031805e9e147217620eada49e280
jiri132 <76393975+jiri132@users.noreply.github.com>
2022-12-20 12:06:36 +0100

    Added: gate inputs and outputs numbers

--- Assets/Scripts/SaveSystem/GateData.cs
+++ Assets/Scripts/SaveSystem/GateData.cs
@@ -9,27 +9,36 @@ using System.Linq;
 [System.Serializable]
 public class GateData 
 {
-    public int NumberOfGates;
+    //Name of the gate and the color
     public string Name;
     public float[] rgb = new float[3];
 
-    
-    public Dictionary<int, TYPES> GateSpawnFormat = new Dictionary<int, TYPES>();
+    //The number of inputs used for the gates, inputs, outputs
+    public int NumberOfGates;
+    public int NumberOfIputs;
+    public int NumberOfOutputs;
+
+    //format <ID,gate name> 
+    public Dictionary<int, string> GateSpawnFormat = new Dictionary<int, string>();
     //format <ID,Output of ID, Conneciton ID, input of connection>
     public List<Tuple<int, int, int, int>> Connections = new List<Tuple<int, int, int, int>>();
 
-    public GateData(Enviorment env)
+   
+    public GateData()
     {
+        Name = Enviorment.Instance.gateNameInput.text;
+        rgb = new float[3] { Enviorment.Instance.randomColor.r, Enviorment.Instance.randomColor.g, Enviorment.Instance.randomColor.b };
+
         NumberOfGates = GameManager.Instance.AllGates.Count;
-        Name = env.name;
-        rgb = new float[3] { env.randomColor.r, env.randomColor.g, env.randomColor.b };
+        NumberOfIputs = Enviorment.Instance.;
+        NumberOfOutputs = ;
 
 
         //Add the Dictionary
         for (int i = 0; i < NumberOfGates; i++)
         {
             LogicComponent gate = GameManager.Instance.AllGates[i];
-            GateSpawnFormat.Add(i, gate.Type);
+            GateSpawnFormat.Add(i, gate.name);
         }
 
 
@@ -52,10 +61,19 @@ public class GateData
                     //copy form the relation gate
                     LogicComponent relationGate = _relation[j].inputNode.ownGate;
 
+
+                    
+
+                    Debug.Log(i);
+                    Debug.Log(j);
+                    Debug.Log(relationGate.ID);
+                    Debug.Log(_relation[j].inputNode.nodeID);
+
                     int ID = i; // i is the ID from the gate
                     int IDOutput = j; // j is the ID of output node
                     int ConnectionID = relationGate.ID; // the relations gate ID
                     int ConnectionIDInput = _relation[j].inputNode.nodeID; // the connection to inputnode ID
+
 
                     Connections.Add(new Tuple<int, int, int, int>(
                         ID,
